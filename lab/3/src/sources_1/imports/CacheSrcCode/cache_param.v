bcf68621ac5149898791d89818ab21c173d18160
totoro <yyw953834564@gmail.com>
2021-05-28 15:30:02 +0800

    lab3 init

---
+++ lab/3/src/sources_1/imports/CacheSrcCode/cache_param.v
@@ -0,0 +1,11 @@
+`ifndef CACHE_CONST_VALUES
+`define CACHE_CONST_VALUES
+//ALUContrl[3:0]
+    `define C_LINE_ADDR_LEN  3
+    `define C_SET_ADDR_LEN   3
+    `define C_TAG_ADDR_LEN   6
+    `define C_WAY_CNT        4
+    `define C_WAY_LEN        2
+    `define POLICY           LRU
+    
+`endif
